f1ede1560c0da28511f446f9ad2c9a920170752f
Jean Parpaillon <jean@parpaillon.info>
2026-09-07 15:05:51 +0200

    Bump default elixir and erlang versions
    CI should take care of retro-compatibility, if any
    Prefer to use latest elixir and erlang versions for developing

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule ExDbus.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :dbus]
+      extra_applications: [:logger]
     ]
   end
 
